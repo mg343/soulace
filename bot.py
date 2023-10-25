3fcec17856e0d03fb68efc25e8a4633e9ef1c17c
mg343 <milu.garimella@gmail.com>
2023-10-25 18:14:25 -0400

    updated main file

--- bot.py
+++ bot.py
@@ -4,22 +4,3 @@ from langchain.prompts import PromptTemplate
 from langchain.memory import ConversationBufferWindowMemory
 from langchain.chains.conversation.memory import ConversationKGMemory
 
-def q_a(inputp):
-    template = """You will respond as a helpful, accurate, friendly, concise, and kind mental health assistant who responds to prompts with warmness and acceptance, while helping to the best of your ability. You will act within your limits to help, as such do not generate false information, and if an answer is not known, you will respond by suggesting for the seeking of help from professionals.
-
-    Human: {input}
-    Assistant:"""
-
-    prompt = PromptTemplate(input_variables=["input"], template=template)
-
-
-    chatgpt_chain = LLMChain(
-        llm=OpenAI(temperature=0,max_tokens=256),
-        prompt=prompt,
-        verbose=True
-    )
-
-    output = chatgpt_chain.predict(
-        input=inputp
-    )
-    return output
--- new_main.py
+++ new_main.py
@@ -1,14 +1,31 @@
 # Bring in deps
-import os 
-from bot import q_a
-from apiKey_noPush import apikey
-os.environ['OPENAI_API_KEY'] = apikey
-
-import streamlit as st 
 from langchain.llms import OpenAI
-from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
 import streamlit as st
+
+
+def q_a(inputp):
+    template = """You will respond as a helpful, accurate, friendly, concise, and kind mental health assistant who responds to prompts with warmness and acceptance, while helping to the best of your ability. You will act within your limits to help, as such do not generate false information, and if an answer is not known, you will respond by suggesting for the seeking of help from professionals.
+
+    Human: {input}
+    Assistant:"""
+
+    prompt = PromptTemplate(input_variables=["input"], template=template)
+
+
+    chatgpt_chain = LLMChain(
+        llm=OpenAI(temperature=0,max_tokens=256),
+        prompt=prompt,
+        verbose=True
+    )
+
+    output = chatgpt_chain.predict(
+        input=inputp
+    )
+    return output
+
+
 
 # App framework
 st.title('GPT App')
