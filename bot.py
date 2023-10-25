f6dfbfcf6d95d2a218509053729b5f218768fc8b
Mihir <milu.garimella@gmail.com>
2023-10-25 17:48:49 -0400

    reverted to older commit

--- bot.py
+++ bot.py
@@ -7,51 +7,19 @@ from langchain.chains.conversation.memory import ConversationKGMemory
 def q_a(inputp):
     template = """You will respond as a helpful, accurate, friendly, concise, and kind mental health assistant who responds to prompts with warmness and acceptance, while helping to the best of your ability. You will act within your limits to help, as such do not generate false information, and if an answer is not known, you will respond by suggesting for the seeking of help from professionals.
 
-    {history}
-    Human: {human_input}
+    Human: {input}
     Assistant:"""
 
-    prompt = PromptTemplate(input_variables=["history", "human_input"], template=template)
+    prompt = PromptTemplate(input_variables=["input"], template=template)
 
 
     chatgpt_chain = LLMChain(
-        llm=OpenAI(model_name="gpt-3.5-turbo-instruct",temperature=0,max_tokens=256),
+        llm=OpenAI(temperature=0,max_tokens=256),
         prompt=prompt,
-        verbose=True,
-        memory=ConversationBufferWindowMemory(k=2),
+        verbose=True
     )
 
     output = chatgpt_chain.predict(
-        human_input=inputp
-    )
-    return output
-
-
-
-def init_model():
-
-    llm=OpenAI(model_name="gpt-3.5-turbo-instruct",temperature=0,max_tokens=256),
-
-    template = """You will respond as a helpful, accurate, friendly, concise, and kind mental health assistant who responds to prompts with warmness and acceptance, while helping to the best of your ability. You will act within your limits to help, as such do not generate false information, and if an answer is not known, you will respond by suggesting for the seeking of help from professionals. You will only use information contained in the "Relevant Information" section and will not hallucinate.
-
-    Relevant Information:
-
-    {history}
-
-    Human: {human_input}
-    Assistant:"""
-
-    prompt = PromptTemplate(input_variables=["history", "human_input"], template=template)
-
-
-    chatgpt_chain = ConversationChain(
-        llm=llm,
-        prompt=prompt,
-        verbose=True,
-        memory=ConversationKGMemory(llm=llm),
-    )
-
-    output = chatgpt_chain.predict(
-        human_input=inputp
+        input=inputp
     )
     return output
--- new_main.py
+++ new_main.py
@@ -1,44 +1,20 @@
 # Bring in deps
-import os
+import os 
+from bot import q_a
 from apiKey_noPush import apikey
-import streamlit as st
-from langchain.llms import OpenAI
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain, ConversationChain  
-from langchain.chains.conversation.memory import ConversationKGMemory
-
 os.environ['OPENAI_API_KEY'] = apikey
 
-# Model 
-llm = OpenAI(model_name='text-davinci-003', temperature=0, max_tokens=256)
+import streamlit as st 
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+import streamlit as st
 
-# Prompt 
-template = """You will respond as a helpful, accurate, friendly, concise, and kind mental health assistant who responds to prompts with warmness and acceptance, while helping to the best of your ability. You will act within your limits to help, as such do not generate false information, and if an answer is not known, you will respond by suggesting for the seeking of help from professionals. You will only use information contained in the "Relevant Information" section and will not hallucinate. Relevant Information: {history} Human: {input} Assistant:"""
+# App framework
+st.title('GPT App')
+prompt = st.text_input('Ask any question about mental health here:', max_chars=256) 
 
-prompt = PromptTemplate(input_variables=["history", "input"], template=template)
-
-# Chain
-chatgpt_chain = ConversationChain(
-    llm=llm, 
-    prompt=prompt,
-    memory=ConversationKGMemory(llm=llm)  
-)
-
-# App
-st.title('Mental Health Chat')
-
-if "messages" not in st.session_state:
-    st.session_state.messages = [{"role": "assistant", "content": "Hi, I'm here to chat about mental health."}]
-
-user_input = st.text_input("You: ")
-
-if user_input:
-
-  st.session_state.messages.append({"role": "user", "content": user_input})
-  with st.spinner("Thinking..."):
-    response = chatgpt_chain.predict(st.session_state.messages)
-
-  st.session_state.messages.append({"role": "assistant", "content": response})
-
-  for msg in st.session_state.messages: 
-    st.write(msg["content"], msg["role"])
+# Show stuff to the screen if there's a prompt
+if prompt: 
+    response = q_a(prompt)
+    st.write(response) 
